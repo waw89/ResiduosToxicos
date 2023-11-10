9b8d5f2f46e5b161f08c3686619946323426f757
waw89 <lcontrerasperaza99@gmail.com>
2023-11-09 21:57:16 -0700

    Persistence Unit

--- ResiduoPersistencia/src/main/java/com/dto/DTOResiduo.java
+++ ResiduoPersistencia/src/main/java/com/dto/DTOResiduo.java
@@ -12,5 +12,38 @@ public class DTOResiduo {
     private long id;
     private String codigo;
     private String nombre;
+
+    public DTOResiduo() {
+    }
+
+    public DTOResiduo(long id, String codigo, String nombre) {
+        this.id = id;
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
