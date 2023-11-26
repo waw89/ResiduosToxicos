10dfdaa4a6f530ec917cbe2383ce2d7c8276cec0
waw89 <lcontrerasperaza99@gmail.com>
2023-11-26 03:58:57 -0700

    Tercer caso de uso casi finalizado

--- ResiduoNegocio/src/main/java/com/validaciones/SolicitudNegocio.java
+++ ResiduoNegocio/src/main/java/com/validaciones/SolicitudNegocio.java
@@ -29,6 +29,7 @@ public class SolicitudNegocio {
     Especificacion_ResiduosJpaController especificacionDAO = new Especificacion_ResiduosJpaController();
     Util util = new Util();
     ISolicitudTrasladoDAO sdao = new SolicitudTrasladoDAOImp();
+    Solicitud_TransportistaJpaController soicitudTransportista_transportistaDAO = new Solicitud_TransportistaJpaController();
 
     public SolicitudTrasladoModel guardar(DTOSolicitaTraslado dtoSolicitaTraslado) {
         SolicitudTrasladoModel st = util.convertirSolicitudTrasladoDTOaSolicitudTraslado(dtoSolicitaTraslado);
@@ -39,35 +40,33 @@ public class SolicitudNegocio {
     public SolicitudTrasladoModel actualizar(DTOSolicitaTraslado dtoSolicitaTraslado, Especificacion_Residuos especificacion) {
         SolicitudTrasladoModel st = util.convertirSolicitudTrasladoDTOaSolicitudTraslado(dtoSolicitaTraslado);
         try {
-                especificacionDAO.edit(especificacion);
-            } catch (Exception e) {
+            especificacionDAO.edit(especificacion);
+        } catch (Exception e) {
 
-            }
+        }
 //        repartirCantidad(st);
         sdao.update(st);
         return st;
     }
-    
+
     /**
-     * Método repartirCantidad que reparte la cantidad del residuo entre las empresas transportistas
+     * Método repartirCantidad que reparte la cantidad del residuo entre las
+     * empresas transportistas
+     *
      * @param listaTransportistas
      * @param especificacion
      * @param numeroEmpresas
      */
-    public void repartirCantidad(List<TransportistaModel> listaTransportistas, Especificacion_Residuos especificacion){
-        Solicitud_Transportista solicitudTransportista = new Solicitud_Transportista();
-        float cantidad = especificacion.getCantidad() / listaTransportistas.size();
-        
-        solicitudTransportista.setCantidad(cantidad);
-        Solicitud_TransportistaJpaController solicitudDAO = new Solicitud_TransportistaJpaController();
-        
-        try{
-            solicitudDAO.edit(solicitudTransportista);
-        }catch(Exception e){
-            
+    public void repartirCantidad(List<TransportistaModel> listaTransportistas, Especificacion_Residuos especificacion) {
+        List<Solicitud_Transportista> registrosSolicituTransportista = this.soicitudTransportista_transportistaDAO.findSolicitud_TransportistaEntities();
+        for (Solicitud_Transportista registro : registrosSolicituTransportista) {
+            registro.setCantidad(especificacion.getCantidad() / listaTransportistas.size());
+            try {
+                this.soicitudTransportista_transportistaDAO.edit(registro);
+            } catch (Exception e) {
+            }
         }
-        
-        
+
     }
 
     /**
@@ -101,19 +100,18 @@ public class SolicitudNegocio {
         return true;
     }
 
- 
-    public List<Especificacion_Residuos> obtenerListaEspecificacionesResiduos(){
+    public List<Especificacion_Residuos> obtenerListaEspecificacionesResiduos() {
         List<Especificacion_Residuos> listaEspecificacionResiduos = new ArrayList<>();
 //        if(especificacionDAO.getEspecificacion_ResiduosCount() == 0){
-            int i = especificacionDAO.getEspecificacion_ResiduosCount();
-            
-            for (int n = 0; n <= i; n++) {
-                if (especificacionDAO.findEspecificacion_Residuos((long) n) != null) {
-                    Especificacion_Residuos especificacionResiduo = especificacionDAO.findEspecificacion_Residuos((long) n);
-                    listaEspecificacionResiduos.add(especificacionResiduo);
-                }
+        int i = especificacionDAO.getEspecificacion_ResiduosCount();
 
+        for (int n = 0; n <= i; n++) {
+            if (especificacionDAO.findEspecificacion_Residuos((long) n) != null) {
+                Especificacion_Residuos especificacionResiduo = especificacionDAO.findEspecificacion_Residuos((long) n);
+                listaEspecificacionResiduos.add(especificacionResiduo);
             }
+
+        }
 //        }else{
 //            int i = especificacionDAO.getEspecificacion_ResiduosCount()-1;
 //            
@@ -125,108 +123,103 @@ public class SolicitudNegocio {
 //
 //            }
 //        }
-         
-        
-        
+
         return listaEspecificacionResiduos;
-        
+
     }
+
     public void actualizaCantidadDelResiduo(List<Float> cantidadesResiduos) {
-        if(especificacionDAO.getEspecificacion_ResiduosCount() == 0){
+        if (especificacionDAO.getEspecificacion_ResiduosCount() == 0) {
             int i = especificacionDAO.getEspecificacion_ResiduosCount();
-            
-            Especificacion_Residuos registroEspecificacion; 
-        for (Float cantidad : cantidadesResiduos) {
-            
-            registroEspecificacion = especificacionDAO.findEspecificacion_Residuos((long) i);
-            
-            registroEspecificacion.setCantidad(cantidad);
-            
-            try {
-                especificacionDAO.edit(registroEspecificacion);
-            } catch (Exception e) {
 
+            Especificacion_Residuos registroEspecificacion;
+            for (Float cantidad : cantidadesResiduos) {
+
+                registroEspecificacion = especificacionDAO.findEspecificacion_Residuos((long) i);
+
+                registroEspecificacion.setCantidad(cantidad);
+
+                try {
+                    especificacionDAO.edit(registroEspecificacion);
+                } catch (Exception e) {
+
+                }
+                i++;
             }
-            i++;
-        }
-        }else{
+        } else {
             int i = especificacionDAO.getEspecificacion_ResiduosCount() - 1;
-            Especificacion_Residuos registroEspecificacion; 
-        for (Float cantidad : cantidadesResiduos) {
-            
-            registroEspecificacion = especificacionDAO.findEspecificacion_Residuos((long) i);
-            
-            registroEspecificacion.setCantidad(cantidad);
-            
-            try {
-                especificacionDAO.edit(registroEspecificacion);
-            } catch (Exception e) {
+            Especificacion_Residuos registroEspecificacion;
+            for (Float cantidad : cantidadesResiduos) {
 
+                registroEspecificacion = especificacionDAO.findEspecificacion_Residuos((long) i);
+
+                registroEspecificacion.setCantidad(cantidad);
+
+                try {
+                    especificacionDAO.edit(registroEspecificacion);
+                } catch (Exception e) {
+
+                }
+                i++;
             }
-            i++;
         }
-        }
-       
 
     }
-    
-public boolean validaSolicitudNoExistente(DTOSolicitaTraslado dtoSolicitudARegistrar) {
-    SolicitudTrasladoModel solicitudARegistrar = util.convertirSolicitudTrasladoDTOaSolicitudTraslado(dtoSolicitudARegistrar);
-    List<SolicitudTrasladoModel> solicitudes = sdao.findSolicitudTrasladoModelEntities();
 
-    boolean solicitudNoExistente = true;
+    public boolean validaSolicitudNoExistente(DTOSolicitaTraslado dtoSolicitudARegistrar) {
+        SolicitudTrasladoModel solicitudARegistrar = util.convertirSolicitudTrasladoDTOaSolicitudTraslado(dtoSolicitudARegistrar);
+        List<SolicitudTrasladoModel> solicitudes = sdao.findSolicitudTrasladoModelEntities();
 
-    for (SolicitudTrasladoModel solicitudActual : solicitudes) {
-        if (solicitudActual == null || solicitudARegistrar == null) {
-            return false; // Manejo de error o lanzar una excepción según tu lógica de negocio
-        }
+        boolean solicitudNoExistente = true;
 
-        LocalDate fechaSolicitudRegistrada = solicitudActual.getFecha();
+        for (SolicitudTrasladoModel solicitudActual : solicitudes) {
+            if (solicitudActual == null || solicitudARegistrar == null) {
+                return false; // Manejo de error o lanzar una excepción según tu lógica de negocio
+            }
 
-        if (fechaSolicitudRegistrada.equals(solicitudARegistrar.getFecha())) {
-            JOptionPane.showMessageDialog(null, "Ya tienes una solicitud para esta fecha, selecciona otra");
-            return false;
-        }
+            LocalDate fechaSolicitudRegistrada = solicitudActual.getFecha();
 
-        List<ResiduoModel> listaResiduoRegistrado = solicitudActual.getListaResiduos();
-        List<ResiduoModel> listaResiduoARegistrar = solicitudARegistrar.getListaResiduos();
+            if (fechaSolicitudRegistrada.equals(solicitudARegistrar.getFecha())) {
+                JOptionPane.showMessageDialog(null, "Ya tienes una solicitud para esta fecha, selecciona otra");
+                return false;
+            }
 
-        if (listaResiduoRegistrado.size() == listaResiduoARegistrar.size()) {
-            // Solo verifica si las listas tienen la misma longitud
-            boolean mismaCombinacion = true;
+            List<ResiduoModel> listaResiduoRegistrado = solicitudActual.getListaResiduos();
+            List<ResiduoModel> listaResiduoARegistrar = solicitudARegistrar.getListaResiduos();
 
-            for (ResiduoModel residuoActualSolicitudARegistrar : listaResiduoARegistrar) {
-                boolean residuoEncontrado = false;
+            if (listaResiduoRegistrado.size() == listaResiduoARegistrar.size()) {
+                // Solo verifica si las listas tienen la misma longitud
+                boolean mismaCombinacion = true;
 
-                for (ResiduoModel residuoActualSolicitudRegistrado : listaResiduoRegistrado) {
-                    // Ajusta la comparación según tu lógica específica para los residuos
-                    if (residuoActualSolicitudARegistrar.getNombre().equalsIgnoreCase(residuoActualSolicitudRegistrado.getNombre())) {
-                        residuoEncontrado = true;
-                        break; // Si se encuentra un residuo, no es necesario seguir buscando
+                for (ResiduoModel residuoActualSolicitudARegistrar : listaResiduoARegistrar) {
+                    boolean residuoEncontrado = false;
+
+                    for (ResiduoModel residuoActualSolicitudRegistrado : listaResiduoRegistrado) {
+                        // Ajusta la comparación según tu lógica específica para los residuos
+                        if (residuoActualSolicitudARegistrar.getNombre().equalsIgnoreCase(residuoActualSolicitudRegistrado.getNombre())) {
+                            residuoEncontrado = true;
+                            break; // Si se encuentra un residuo, no es necesario seguir buscando
+                        }
+                    }
+
+                    if (residuoEncontrado) {
+                        mismaCombinacion = false;
+                        JOptionPane.showMessageDialog(null, "Ya tienes una solicitud con estos residuos, selecciona otros");
+                        return false; // Si un residuo se encuentra, la combinación no es la misma
                     }
                 }
 
-                if (residuoEncontrado) {
-                    mismaCombinacion = false;
-                    JOptionPane.showMessageDialog(null, "Ya tienes una solicitud con estos residuos, selecciona otros");
-                    return false; // Si un residuo se encuentra, la combinación no es la misma
+                if (mismaCombinacion) {
+                    return false; // La combinación de residuos ya existe en otra solicitud, retorna false
                 }
             }
 
-            if (mismaCombinacion) {
-                return false; // La combinación de residuos ya existe en otra solicitud, retorna false
+            if (!solicitudNoExistente) {
+                return false; // Si se encontró una coincidencia, termina la verificación y retorna false
             }
         }
 
-        if (!solicitudNoExistente) {
-            return false; // Si se encontró una coincidencia, termina la verificación y retorna false
-        }
+        return solicitudNoExistente; // Retorna el resultado final
     }
 
-    return solicitudNoExistente; // Retorna el resultado final
 }
-
- 
-    
-}
-
